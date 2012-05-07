a51f970969b9c6f087508b95ee5038d63090df95
Craig Shyjak <cshyjak@tellusaboutus.com>
2012-05-07 11:40:35 -0500

    Update to latest service stack binaries

--- src/ServiceStack.ContainerAdapter.Ninject/NinjectContainerAdapter.vb
+++ src/ServiceStack.ContainerAdapter.Ninject/NinjectContainerAdapter.vb
@@ -1,6 +1,5 @@
 ﻿Imports Ninject
 Imports Ninject.Parameters
-Imports ServiceStack.ServiceHost
 Imports ServiceStack.Configuration
 
 Public Class NinjectContainerAdapter
@@ -12,11 +11,11 @@ Public Class NinjectContainerAdapter
         _kernel = kernel
     End Sub
 
-    Public Function Resolve(Of T)() As T Implements Configuration.IContainerAdapter.Resolve
+    Public Function Resolve(Of T)() As T Implements IContainerAdapter.Resolve
         Return _kernel.Get(Of T)()
     End Function
 
-    Public Function TryResolve(Of T)() As T Implements Configuration.IContainerAdapter.TryResolve
+    Public Function TryResolve(Of T)() As T Implements IContainerAdapter.TryResolve
         If _kernel.CanResolve(_kernel.CreateRequest(GetType(T), Function(x) True, New List(Of IParameter), False, False)) Then
             Return _kernel.Get(Of T)()
         Else
